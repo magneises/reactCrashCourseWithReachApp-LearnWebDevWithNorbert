43cd795e5fa772722356840f62c568afb7af4ac6
Preston G <prestongilmore@gmail.com>
2025-02-16 13:10:46 -0700

    updated App.css styling

--- workout-app/src/App.jsx
+++ workout-app/src/App.jsx
@@ -2,18 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import yogaPose2 from './images/yoga-pose-2.png'
-
 import './App.css'
 
 function App() {
+  // Keeping below for reference styling
+  // let h1Style = {
+  //   color: "red",
+  //   backgroundColor: "blue",
+  // }
+
+
   return <div className="App">
     <h1 className="text-purple">Hello, this is the App.jsx page</h1>
       <header>
         <h2>Welcome to the</h2>
         <h1>Workout App</h1>
-        <img src={yogaPose2} alt="" />
-
       </header>
+      <main>
+        <div className="col">
+          <img src={yogaPose2} alt="homeimg" />
+        </div>
+      </main>
   </div>
 }
 
