1528af9e8e1027dca628ea7dddd345845475b679
Preston G <prestongilmore@gmail.com>
2025-02-18 00:16:00 -0700

    updated app.jsx, created nav and included icons

--- workout-app/src/App.jsx
+++ workout-app/src/App.jsx
@@ -1,7 +1,10 @@
 
+import './App.css'
+import { FaHome, FaUser, FaChartLine } from "react-icons/fa"
+import { BsPlusCircleFill, BsBellFill } from "react-icons/bs"
 import Title from "./components/Title"
 import Home from "./components/Home"
-import './App.css'
+
 
 function App() {
   // Keeping below for reference styling
@@ -10,6 +13,13 @@ function App() {
   //   backgroundColor: "blue",
   // }
   return <div className="App">
+    <nav className="navbar">
+      <li><FaHome/></li>
+      <li><FaUser/></li>
+      <li><BsPlusCircleFill/></li>
+      <li><FaChartLine/></li>
+      <li><BsBellFill/></li>
+    </nav>
     <Home />
   </div>
 }
