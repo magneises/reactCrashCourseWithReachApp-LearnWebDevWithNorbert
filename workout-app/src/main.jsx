d11fae544311840f386265714e3736dd4d48aaab
Preston G <prestongilmore@gmail.com>
2025-02-18 21:30:59 -0700

    update

--- workout-app/src/main.jsx
+++ workout-app/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import Home from './routes/Home.jsx';
 import Workouts from './routes/Workouts.jsx';
+import { FaArrowUp } from 'react-icons/fa'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
@@ -12,7 +13,13 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="home" element={<Home />} />
+
           <Route path='workout' element={<Workouts />} />
+            {" "}
+            <Route 
+              index 
+              element={<main className="card bg-orange m-3">Please Select a Day</main>} 
+            />
           <Route 
             path="*" 
             element={
@@ -24,5 +31,5 @@ createRoot(document.getElementById('root')).render(
         </Route>
       </Routes>
     </Router>
-  </StrictMode>,
+  </StrictMode>
 );
