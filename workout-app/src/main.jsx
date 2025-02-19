d06b2fb27d068ad7f517eae00326613e906e7110
Preston G <prestongilmore@gmail.com>
2025-02-18 19:44:38 -0700

    created Workouts.jsx components

--- workout-app/src/main.jsx
+++ workout-app/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import Home from './routes/Home.jsx';
+import Workouts from './routes/Workouts.jsx';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="home" element={<Home />} />
+          <Route path='workout' element={<Workouts />} />
           <Route 
             path="*" 
             element={
